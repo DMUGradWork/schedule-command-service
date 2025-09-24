8b1515d1796424be35fc664f6c9beed2d7ff27a5
ghkdtjfxkd <syt1128@gmail.com>
2025-09-24 10:30:31 +0900

    feat: scheduleId·ownerId로 조회 메서드 추가

--- src/main/java/grewmeet/schedulecommandservice/repository/ScheduleRepository.java
+++ src/main/java/grewmeet/schedulecommandservice/repository/ScheduleRepository.java
@@ -2,7 +2,9 @@ package grewmeet.schedulecommandservice.repository;
 
 import grewmeet.schedulecommandservice.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    Optional<Schedule> findByScheduleIdAndOwnerId(UUID scheduleId, UUID ownerId);
 }
-
